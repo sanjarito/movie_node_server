08b9ec9cb5fa26899e6c3d76d361b2dc0b55ba5a
Santiago Casar <santiago@nowhereprod.com>
2019-07-29 11:39:42 -0400

    all

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 const app = express()
-console.log(process.env.API_TOKEN)
-
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
-   console.log('validate bearer token middleware')
+
    // move to the next middleware
    const authToken = req.get('Authorization')
    const apiToken = process.env.API_TOKEN
@@ -45,8 +44,17 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/movie', handleGetMovie)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
